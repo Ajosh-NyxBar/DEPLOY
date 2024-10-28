00cdfd871bbc202ee65ec02f4dc91db234fc20a6
MuhammadRifkyAkbar <2210631170088@student.unsika.ac.id>
2024-10-28 12:26:15 +0700

    init

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "" // TODO: replace this
+const studentName = "Muhammad Rifky Akbar" // TODO: replace this
+const studentId = "FS9491639" // TODO: replace this
+
+export { url, studentName, studentId }
